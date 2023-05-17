0482778c05413bc16ce02ad95020fdc41c48cf98
valeriia rieznikova <72576722+valeria-rezn@users.noreply.github.com>
2023-05-17 14:11:03 +0300

    Update tennis1.cc
    Add map for score name, simplified the conditions

--- cpp/tennis1.cc
+++ cpp/tennis1.cc
@@ -1,58 +1,43 @@
 #include <string>
-
-const std::string tennis_score(int p1Score, int p2Score) {
-    std::string score = "";
-    int tempScore=0;
-    if (p1Score==p2Score)
+#include <map>
+const std::string tennis_score(int p1_score, int p2_score) {
+    std::string score = ""; 
+    const std::map<int, std::string> score_name = 
     {
-        switch (p1Score)
-        {
-            case 0:
-                    score = "Love-All";
-                break;
-            case 1:
-                    score = "Fifteen-All";
-                break;
-            case 2:
-                    score = "Thirty-All";
-                break;
-            default:
-                    score = "Deuce";
-                break;
+        {0, "Love"},
+        {1, "Fifteen"},
+        {2, "Thirty"}, 
+        {3, "Forty"}
+    };
 
-        }
+    if (p1_score==p2_score)
+    {
+        if (p1_score >= 3)
+            score = "Deuce";
+        else
+            score = score_name.at(p1_score) + "-All";
     }
-    else if (p1Score>=4 || p2Score>=4)
+    else if (p1_score>=4 || p2_score>=4)
     {
-        int minusResult = p1Score-p2Score;
-        if (minusResult==1) score ="Advantage player1";
-        else if (minusResult ==-1) score ="Advantage player2";
-        else if (minusResult>=2) score = "Win for player1";
-        else score ="Win for player2";
+        
+        int diff = p1_score-p2_score;
+        switch (diff)
+        {
+            case 1: score = "Advantage player1"; break;
+            case -1: score = "Advantage player2"; break;
+            default:
+                if (diff > 0)
+                    score = "Win for player1";
+                else
+                    score = "Win for player2";
+                break;
+        }
     }
     else
     {
-        for (int i=1; i<3; i++)
-        {
-            if (i==1) tempScore = p1Score;
-            else { score+="-"; tempScore = p2Score;}
-            switch(tempScore)
-            {
-                case 0:
-                    score+="Love";
-                    break;
-                case 1:
-                    score+="Fifteen";
-                    break;
-                case 2:
-                    score+="Thirty";
-                    break;
-                case 3:
-                    score+="Forty";
-                    break;
-            }
-        }
+        score = score_name.at(p1_score) + "-" + score_name.at(p2_score);
     }
+
     return score;
     
 }
