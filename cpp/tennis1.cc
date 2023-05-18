3b61a8e9ccc62aad1f1d0d746018744edd917696
valeriia rieznikova <72576722+valeria-rezn@users.noreply.github.com>
2023-05-18 19:57:41 +0300

    Update tennis1.cc for using "tennis" library

--- cpp/tennis1.cc
+++ cpp/tennis1.cc
@@ -1,43 +1,8 @@
-#include <string>
-#include <map>
+#include "tennis.h"
+
 const std::string tennis_score(int p1_score, int p2_score) {
-    std::string score = ""; 
-    const std::map<int, std::string> score_name = 
-    {
-        {0, "Love"},
-        {1, "Fifteen"},
-        {2, "Thirty"}, 
-        {3, "Forty"}
-    };
 
-    if (p1_score==p2_score)
-    {
-        if (p1_score >= 3)
-            score = "Deuce";
-        else
-            score = score_name.at(p1_score) + "-All";
-    }
-    else if (p1_score>=4 || p2_score>=4)
-    {
-        
-        int diff = p1_score-p2_score;
-        switch (diff)
-        {
-            case 1: score = "Advantage player1"; break;
-            case -1: score = "Advantage player2"; break;
-            default:
-                if (diff > 0)
-                    score = "Win for player1";
-                else
-                    score = "Win for player2";
-                break;
-        }
-    }
-    else
-    {
-        score = score_name.at(p1_score) + "-" + score_name.at(p2_score);
-    }
-
+    std::string score = get_tennis_score(p1_score, p2_score);
     return score;
     
 }
